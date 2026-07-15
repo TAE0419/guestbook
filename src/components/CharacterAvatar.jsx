8c0997ee8944a10f08f16e10a2c52dee8e813f6f
TAE0419 <ktcat0419@gmail.com>
2026-07-15 09:28:18 +0900

    initial commit

--- src/components/CharacterAvatar.jsx
+++ src/components/CharacterAvatar.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import CHARACTERS from './characterData'
-import styles from './characterAvatar.module.scss'
+import styles from './CharacterAvatar.module.scss'
 
 const CharacterAvatar = ({character}) => {
   const characterData = CHARACTERS.find((item)=>item.id === character)
